ae981022f12dd529c31c5e494f20444db45540ff
san4ezz87 <san4ezz.8989@gmail.com>
2021-05-06 17:51:39 +0300

    шаблонизируй это (#11)

--- src/express/express.js
+++ src/express/express.js
@@ -1,17 +1,36 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
 const app = express();
+const {HttpCode} = require(`../utils/httpCodes`);
+
 const APPLICATION_PORT = process.env.PORT || 8080;
 
-const mainRoutes = require(`./routes/mainRoutes`);
-const myRoutes = require(`./routes/myRoutes`);
-const articlesRouters = require(`./routes/articlesRouters`);
+const mainRoutes = require(`./routes/main-routes`);
+const myRoutes = require(`./routes/my-routes`);
+const articlesRouters = require(`./routes/articles-routers`);
+
+app.set(`view engine`, `pug`);
+app.set(`views`, path.resolve(__dirname + `/templates`));
+
+app.use(express.static(path.resolve(__dirname + `/public`)));
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/articles`, articlesRouters);
 
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND).render(`errors/404`);
+});
+
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`errors/500`);
+  }
+  next();
+});
+
 app.listen(APPLICATION_PORT, () => {
-  console.log(`Example app listening at http://localhost:${APPLICATION_PORT}`)
+  console.log(`Example app listening at http://localhost:${APPLICATION_PORT}`);
 });
