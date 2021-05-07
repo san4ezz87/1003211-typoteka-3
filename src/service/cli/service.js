adfb7e7fb2e15cc0470210c6e1c1ac3596b240b0
san4ezz87 <san4ezz.8989@gmail.com>
2021-05-07 07:38:12 +0300

    другой сервер (#12)

--- src/service/cli/service.js
+++ src/service/cli/service.js
@@ -1,68 +1,48 @@
 'use strict';
-const chalk = require(`chalk`);
-const http = require(`http`);
+
+const express = require(`express`);
+const app = express();
+
+
 const fs = require(`fs`).promises;
 const path = require(`path`);
 const {HttpCode} = require(`../../utils/httpCodes`);
 
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
-const notFoundMessageText = `Not found`;
-
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-  <!Doctype html>
-    <html lan="ru">
-    <head>
-      <title>With love from Node</title>
-    </head>
-    <body>${message}</body>
-  </html>`.trim();
-
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
 
 const preparePath = (url) => path.resolve(__dirname, `../../../`, url);
 
-const onClientConnect = async (req, res) => {
-  switch (req.url) {
-    case `/`: {
-      try {
-        const fileContent = await fs.readFile(preparePath(FILENAME));
-        const mocks = JSON.parse(fileContent);
+app.use(express.json());
 
-        const li = mocks.map((elem) => {
-          return `<li>${elem.title}</li>`;
-        }).join(``);
-
-        const list = `<ul>${li}</ul>`;
-        sendResponse(res, HttpCode.OK, list);
-      } catch (e) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-      break;
-    }
-    default: {
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-    }
+app.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(preparePath(FILENAME));
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (err) {
+    res.json([]);
   }
-};
+});
+
+app.use((req, res) => {
+  res.status(HttpCode.NOT_FOUND).end(`404`);
+});
+
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).end(`500`);
+  }
+  next();
+});
 
 module.exports = {
   name: `--server`,
   run([customPort]) {
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
-    http.createServer(onClientConnect).listen(port).on(`listening`, (err) => {
-      if (err) {
-        console.log(`Ошибка при создании сервера`, err);
-      }
 
-      console.info(console.log(chalk.green(`Ожидаю соединения на порту ${port}`)));
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`)
     });
   }
 };
