75f2b3bc98519c5a8fea61b574185cdc9d32bff7
Smirnov Aleksandr Vladimirovich <san4ezz.8989@gmail.com>
2022-01-08 16:01:33 +0300

    Закрытые страницы, CSRF-атаки

--- src/express/middlewares/index.js
+++ src/express/middlewares/index.js
@@ -3,9 +3,11 @@
 const upload = require(`./img-storage`);
 const handleServerError = require(`./handle-server-error`);
 const handleClientError = require(`./handle-client-error`);
+const auth = require(`./auth`);
 
 module.exports = {
   upload,
   handleServerError,
   handleClientError,
+  auth,
 };
